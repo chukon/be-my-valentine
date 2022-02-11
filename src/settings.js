9b6aa50f90ebb86c88458e79f817988d08da1dca
Chuck Konkol <ckonkol@gmail.com>
2022-02-11 12:34:21 -0600

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -7,7 +7,14 @@
 /* FIREBASE CONFIG
 -------------------------------- */
 // PASTE YOUR WEB APP FIREBASE CONFIG HERE
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA9VA3LCGeg9j4nwtg2aibgGteq0AYJhEY",
+  authDomain: "bemyvalentine-ca406.firebaseapp.com",
+  projectId: "bemyvalentine-ca406",
+  storageBucket: "bemyvalentine-ca406.appspot.com",
+  messagingSenderId: "892280027079",
+  appId: "1:892280027079:web:b9dece78e4037317f58be2"
+};
 
 /* CLOUD FIRESTORE CONFIG
 -------------------------------- */
@@ -20,8 +27,8 @@ const firestore = {
 /* UI DATA
 -------------------------------- */
 const uiData = {
-    valentinesFirstName: 'Ashley',
-    valentinesLastName: 'Testington',
+    valentinesFirstName: 'Lori',
+    valentinesLastName: 'Konkol',
     headerMessage: 'will you bee my Valentine? 🌹',
     resultHeaderYes: 'You said yes! 😍',
     resultHeaderNo: 'You said no 😢',
